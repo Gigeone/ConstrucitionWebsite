802b641f52408723c40ff4d2af6d78e1dc1854b1
Gigeone <matthieugh@gmail.com>
2024-09-16 19:13:42 +0200

    footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import { FaArrowUp } from "react-icons/fa";
 import { FaCopyright } from "react-icons/fa6";
 import { Link } from "react-scroll";
-import { FaArrowUp } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="bg-black text-white flex justify-center items-center gap-2 p-5">
-      <FaCopyright className="fill-yellow-500 lg:size-5 size-8" />
-      <p className="text-lg text-center">
-        Copyright 2024, Matthieu GH Agency, All Rights Reserved
-      </p>
-    </div>
+    <>
+      <div className="bg-black text-white flex justify-center items-center gap-2 p-5">
+        <FaCopyright className="fill-yellow-500 lg:size-5 size-8" />
+        <p className="text-lg text-center">
+          Copyright 2024, Matthieu GH Agency, All Rights Reserved
+        </p>
+      </div>
+      {/* icon box */}
+      <div
+        id="icon-box"
+        className="bg-yellow-500 text-black p-3 rounded-full hover:bg-black hover:text-white cursor-pointer fixed lg:bottom-6 right-6 bottom-6"
+      >
+        <Link to="hero" spy={true} smooth={true} offset={-100}>
+          <FaArrowUp className="size-6" />
+        </Link>
+      </div>
+    </>
   );
 };
 
